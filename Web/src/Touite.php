45cf650cc23b91101ce34e33454225b961a245e5
Félix <sbotixoxo@gmail.com>
2023-11-06 15:32:58 +0100

    Ajout de la classe Touite.php

---
+++ Web/src/Touite.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+namespace Iutncy\Sae;
+class Touite {
+    private string $texte;
+    private string $date;
+    private User $auteur;
+    private array $liTag;
+}

1b868a61159707ae685611b2ca1883613ddec7cd
Thomas M <micheli17u@etu.univ-lorraine.fr>
2023-11-07 09:09:02 +0100

    tag

--- Web/src/Tag/Tag.php
+++ Web/src/Tag/Tag.php
@@ -10,4 +10,12 @@ class Tag{
         $this->libelle = $libelle;
         $this->description = $description;
     }
+
+    public function __get(string $at): mixed
+    {
+        if (!property_exists($this, $at)) {
+            throw new \iutnc\deefy\exception\InvalidPropertyNameException(get_called_class() . "invalid property : $at");
+        }
+        return $this->$at;
+    }
 }
